7f0aa1ac2c5d409ba47e666a601cc5d6a6006236
Luciano <lucianoa2028@gmail.com>
2021-05-30 05:27:02 -0300

    Tela Login

---
+++ app/src/main/java/br/com/ptz/item/LoginActivity.kt
@@ -0,0 +1,11 @@
+package br.com.ptz.item
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
