20686a9fa8da5a429d5637a94a550926b5ef006f
Luciano <lucianoa2028@gmail.com>
2021-05-31 12:42:15 -0300

    Tela PrecoActivity.kt passagem de paramentros entre activity

--- app/src/main/java/br/com/ptz/item/sistema/FornecedorActivity.kt
+++ app/src/main/java/br/com/ptz/item/sistema/FornecedorActivity.kt
@@ -1,7 +1,10 @@
 package br.com.ptz.item.sistema
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import br.com.ptz.item.EXTRA_MESSAGE
 import br.com.ptz.item.R
@@ -10,6 +13,21 @@ class FornecedorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fornecedor)
+
+
+        // Criando Button
+        val BTNPreco = findViewById<Button>(R.id.btnPreco) as Button
+
+        // Chamando onclicklistenner
+        BTNPreco.setOnClickListener {
+
+
+            val intentPreco = Intent(this, PrecoActivity::class.java).apply {
+            }
+            startActivity(intentPreco)
+
+        }
+
 
         // Get the Intent that started this activity and extract the string
         val message = intent.getStringExtra(EXTRA_MESSAGE)
@@ -20,3 +38,6 @@ class FornecedorActivity : AppCompatActivity() {
 
     }
 }
+
+
+
---
+++ app/src/main/java/br/com/ptz/item/sistema/PrecoActivity.kt
@@ -0,0 +1,12 @@
+package br.com.ptz.item.sistema
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.com.ptz.item.R
+
+class PrecoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preco)
+    }
+}
